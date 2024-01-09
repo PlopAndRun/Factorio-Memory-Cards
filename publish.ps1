9ed6570b6405294d00c06dbdc347ba3f0da55d80
PlopAndRun <PlopAndRun@users.noreply.github.com>
2024-01-09 18:04:13 +0200

    Publish scripts

---
+++ publish.ps1
@@ -0,0 +1,23 @@
+$json = Get-Content .\info.json | ConvertFrom-Json
+$version = $json.version
+$full_name = "memory-cards_$version"
+$publish_dir = ".\publish"
+$out_archive = "$publish_dir\$full_name.zip"
+$excludes = @(".gitignore", ".vscode", ".git", "publish.ps1")
+
+Write-Output "Preparing directory"
+if (!(Test-Path $publish_dir)) {
+    New-Item -ItemType Directory -Path $publish_dir
+}
+if (Test-Path $out_archive) {
+    Write-Output "Deleting existing archive"
+    Remove-Item -Force $out_archive
+}
+
+$items = Get-ChildItem . -Exclude @(".gitignore", ".git", ".vscode", "Publish", "publish.ps1")
+
+Write-Output "Creating archive $out_archive"
+Compress-Archive -Path $items -CompressionLevel Optimal -DestinationPath $out_archive
+
+Write-Output "Publishing archive"
+gh release create "v$version" "$out_archive" --notes "v$version"
