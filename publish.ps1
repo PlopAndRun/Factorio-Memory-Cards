e63ce595766b02d71b39cdfc5b7d457042c42a0f
PlopAndRun <PlopAndRun@users.noreply.github.com>
2024-01-18 13:58:30 +0200

    Fixed release scripts to include the folder name and to exclude gimp source files

--- publish.ps1
+++ publish.ps1
@@ -3,18 +3,23 @@ $version = $json.version
 $full_name = "memory-cards_$version"
 $publish_dir = ".\publish"
 $out_archive = "$publish_dir\$full_name.zip"
+$out_dir = "$publish_dir\$full_name"
 $excludes = @(".gitignore", ".vscode", ".git", "publish.ps1")
 
 Write-Output "Preparing directory"
 if (!(Test-Path $publish_dir)) {
     New-Item -ItemType Directory -Path $publish_dir
 }
+if (Test-Path $out_dir) {
+    Remove-Item -Path $out_dir -Recurse
+}
 if (Test-Path $out_archive) {
     Write-Output "Deleting existing archive"
     Remove-Item -Force $out_archive
 }
 
-$items = Get-ChildItem . -Exclude @(".gitignore", ".git", ".vscode", ".github", "Publish", "publish.ps1", "readme.md")
+$items = Get-ChildItem -Recurse . -Exclude @(".gitignore", ".git", ".vscode", ".github", "Publish", "publish.ps1", "readme.md", "*.xcf")
 
 Write-Output "Creating archive $out_archive"
-Compress-Archive -Path $items -CompressionLevel Optimal -DestinationPath $out_archive
+robocopy . "$out_dir" /XD ".vscode" ".git" ".github" "publish" /XF ".gitignore" "readme.md" "*.xcf" "publish.ps1" /S
+Compress-Archive -Path $out_dir -CompressionLevel Optimal -DestinationPath $out_archive
